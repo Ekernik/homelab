392aa86364876ee5f097215c23c6c1a8d18bad3c
Nikita Ekern <ekernik@control.home.arpa>
2025-09-28 19:55:44 -0400

    refactor: fix url in LXC description.

--- terraform/envs/lab/technitium_dns.tf
+++ terraform/envs/lab/technitium_dns.tf
@@ -1,6 +1,7 @@
 locals {
   ssh_public_key      = file("~/.ssh/id_rsa.pub")
-  service_ip          = "192.168.1.30/24"
+  service_ip          = "192.168.1.30"
+  service_CIDR        = "${local.service_ip}/24"
   service_port        = "5380"
 }
 
@@ -14,12 +15,12 @@ module "technitium_dns_lxc" {
   target_node     = "proxmox2"
   vmid            = 0
   os_type         = "ubuntu"
-  description     = "DNS Server. ${local.service_ip}:${local.service_port}"
+  description     = "Technitium DNS Server. http://${local.service_ip}:${local.service_port}"
   tags            = "terraform;dns"
   hostname        = "dns1.home.arpa"
   ssh_pubkey      = local.ssh_public_key
   disk_size       = "2G"
-  ipv4_address    = local.service_ip
+  ipv4_address    = local.service_CIDR
   ipv4_gateway    = "192.168.1.1"
   root_password   = var.lxc_root_password
 }
