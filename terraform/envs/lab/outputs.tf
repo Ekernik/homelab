6dcf6017c6a9871ed4a8be08a9e159039353bb97
Nikita Ekern <ekernik@control.home.arpa>
2025-09-28 20:12:05 -0400

    feat: add npmplus provisioning to terraform

--- terraform/envs/lab/outputs.tf
+++ terraform/envs/lab/outputs.tf
@@ -1,6 +1,7 @@
 locals {
   hosts = {
-    dns1 = { ip = module.technitium_dns_lxc.ipv4 }
+    dns1    = { ip = module.technitium_dns_lxc.ipv4 }
+    npmplus = { ip = module.nginx_proxy_manager_plus.ipv4 }
   }
 }
 
